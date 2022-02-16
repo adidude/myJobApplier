be17f77481c652bf7589d14e7b2a27cd790b4417
Aditya Kumar Menon <menon.or.adithya@gmail.com>
2022-02-16 01:55:45 +0000

    Added rudimentry scripts to apply to linkedin jobs

---
+++ EasyApply.py
@@ -0,0 +1,93 @@
+import time
+from selenium import webdriver
+from selenium.common import exceptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.touch_actions import TouchActions
+from selenium.common.exceptions import *
+
+PATH = "C:\Program Files (x86)\chromedriver\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.set_window_size(1254,1047)
+driver.get("https://www.linkedin.com")
+driver.find_element_by_id("session_key").send_keys("menon.or.adithya@gmail.com")
+driver.find_element_by_id("session_password").send_keys("hoTJ6ihV^o9")
+driver.find_element_by_css_selector(".sign-in-form__submit-button").click()
+
+
+
+pageLoader = WebDriverWait(driver,300,2,ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException])
+pageLoader.until(EC.element_to_be_clickable((By.CLASS_NAME, "msg-overlay-bubble-header__control")))
+closeMessages = driver.find_elements_by_class_name("msg-overlay-bubble-header__control")
+closeMessages[1].click()
+jobsButton = driver.find_elements_by_class_name("global-nav__primary-link")
+jobsButton[2].click()
+
+pageLoader.until(EC.element_to_be_clickable((By.CLASS_NAME,"jobs-search-box__text-input")))
+inputBoxes = driver.find_elements_by_class_name("jobs-search-box__text-input")
+
+inputBoxes[0].send_keys("graduate software engineer")
+inputBoxes[3].send_keys("United Kingdom")
+
+driver.find_element_by_class_name("jobs-search-box__submit-button").click()
+
+pageLoader.until(EC.element_to_be_clickable((By.CLASS_NAME,"artdeco-pill")))
+options = driver.find_elements_by_class_name("artdeco-pill")
+options[7].click()
+
+easyToggle = pageLoader.until(EC.element_to_be_clickable((By.CLASS_NAME,"artdeco-toggle")))
+easyToggle.click()
+
+driver.find_element_by_class_name("reusable-search-filters-buttons").click()
+
+time.sleep(5)
+
+total_results = driver.find_element_by_class_name("display-flex.t-12.t-black--light.t-normal")
+total_results_int = int(total_results.text.split(' ',1)[0].replace(",",""))
+#print("Jobs available:" + str(total_results_int))
+
+
+
+pageCollector = driver.find_elements_by_class_name("artdeco-pagination__indicator")
+
+print("Number of pages: " + str(len(pageCollector)))
+totalPages = int(pageCollector[len(pageCollector)-1].get_attribute("data-test-pagination-page-btn"))
+
+newCount = 0
+
+for currPage in range(0, totalPages):
+	time.sleep(2)
+	pageCollector = driver.find_elements_by_class_name("artdeco-pagination__indicator")
+
+	if (currPage < 9):
+		pageCollector[currPage].click()
+	else:
+		try:
+			pageCollector[newCount + 6].click()
+			newCount += 1
+			if (newCount > 2):
+				newCount = 0
+		except IndexError:
+			driver.find_element_by_class_name("jobs-search-no-results__reload").click()
+			time.sleep(1)
+			pageCollector[newCount + 6].click()
+			newCount += 1
+			if (newCount > 2):
+				newCount = 0
+
+
+
+
+#isFirst = True
+#for page in pages:
+#	time.sleep(0.5)
+#	jobs = driver.find_elements_by_class_name("jobs-search-results__list-item")
+#	jobs[len(jobs)-1].click()
+#	if (isFirst):
+#		isFirst = False
+#		continue
+#	page.click()
+
+#driver.quit()
---
+++ peasyApply.py
@@ -0,0 +1,70 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import *
+
+PATH = "C:\Program Files (x86)\chromedriver\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+driver.set_window_size(1254,1047)
+driver.get("https://www.linkedin.com/login?emailAddress=&fromSignIn=&fromSignIn=true&session_redirect=https%3A%2F%2Fwww.linkedin.com%2Fjobs%2Fsearch%2F%3Ff_AL%3Dtrue%26geoId%3D101165590%26keywords%3Dgraduate%2520software%2520engineer%26location%3DUnited%2520Kingdom%26sortBy%3DR&trk=public_jobs_nav-header-signin")
+
+driver.find_element_by_id("username").send_keys("menon.or.adithya@gmail.com")
+driver.find_element_by_id("password").send_keys("hoTJ6ihV^o9")
+# Click log in
+driver.find_element_by_class_name("btn__primary--large").click()
+
+# Create webdriverwait to wait for page to load
+pageLoader = WebDriverWait(driver,300,2,ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException])
+pageLoader.until(EC.visibility_of_any_elements_located((By.CLASS_NAME,"artdeco-pagination__indicator")))
+
+# Gets all the page change buttons
+pageCollector = driver.find_elements_by_class_name("artdeco-pagination__indicator")
+
+# The total number of pages
+totalPages = int(pageCollector[len(pageCollector)-1].get_attribute("data-test-pagination-page-btn"))
+
+newCount = 0
+errorChecker = WebDriverWait(driver,5,ignored_exceptions=[ElementNotVisibleException, ElementNotSelectableException])
+
+def openNextPage(currPage, newCount):
+	pageLoader.until(EC.visibility_of_any_elements_located((By.CLASS_NAME, "artdeco-pagination__indicator")))
+	pageCollector = driver.find_elements_by_class_name("artdeco-pagination__indicator")
+
+	if (currPage < 9):
+		pageCollector[currPage].click()
+	else:
+		pageCollector[newCount + 6].click()
+		newCount += 1
+		if (newCount > 2):
+			newCount = 0
+
+for currPage in range(0, totalPages):
+	try:
+		errorChecker.until(EC.element_to_be_clickable((By.CLASS_NAME,"jobs-search-no-results__reload")))
+		driver.find_element_by_class_name("jobs-search-no-results__reload").click()
+		openNextPage(currPage, newCount)
+	except:
+		openNextPage(currPage, newCount)
+		
+		""" 
+		pageLoader.until(EC.visibility_of_any_elements_located((By.CLASS_NAME, "artdeco-pagination__indicator")))
+		pageCollector = driver.find_elements_by_class_name("artdeco-pagination__indicator")
+
+		if (currPage < 9):
+			pageCollector[currPage].click()
+		else:
+			try:
+				pageCollector[newCount + 6].click()
+				newCount += 1
+				if (newCount > 2):
+					newCount = 0
+			except IndexError:
+				driver.find_element_by_class_name("jobs-search-no-results__reload").click()
+				time.sleep(1)
+				pageCollector[newCount + 6].click()
+				newCount += 1
+				if (newCount > 2):
+					newCount = 0
+ 		"""
